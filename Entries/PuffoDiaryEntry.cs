75c86ece06fa7327ef6bf6b2899ed3cfd2484e4e
Bradlee Barnes <69256931+StupidRepo@users.noreply.github.com>
2024-05-06 13:33:31 +0100

    Lol

--- Entries/PuffoDiaryEntry.cs
+++ Entries/PuffoDiaryEntry.cs
@@ -6,7 +6,6 @@ namespace DiaryWarning.Entries;
 public class PuffoDiaryEntry : IDiaryEntry
 {
     public string GetTitle() => "Puffo";
-
     public string GetLore() =>
         "Puffo? Yeah, an interesting species. But then again, they're all interesting. Aren't they? Anyway, Puffos are kinda like... pufferfish. I think that's what they used to call them. 'Pufferfish'. " +
         "We lost someone from the team to a Puffo once. Cornered by 2; bounced around like a pinball. Hilarious it was, until we realised he weren't moving no more. " +
@@ -14,7 +13,7 @@ public class PuffoDiaryEntry : IDiaryEntry
     public IEnumerable<string> GetAbilities() =>
     [
         "Inflates when you get close to it.",
-        "Creates a shockwave, upon inflating, that pushes you back and deals up to <color=red>25</color> damage."
+        "Upon inflating, a Puffo releases a shockwave that pushes you back and deals up to <color=red>25</color> damage."
     ];
     
     public int GetPossibleViews()
